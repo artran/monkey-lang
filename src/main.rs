81a2a3c5c094747aa9a2c3bdafeef8be4f92d5a5
Ray Tran <ray@artran.co.uk>
2024-03-03 16:01:50 +0000

    Make `main` use the Lexer to remove `unused code` errors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 mod lexer;
 mod token;
 
+use lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    let input = r#"
+        let five = 5;
+        let ten = 10;
+        let add = fn(x, y) {
+            x + y;
+        };
+        let result = add(five, ten);
+        "#;
+
+    let mut l = Lexer::new(input.to_string());
+    println!("{:?}", l.next_token());
 }
